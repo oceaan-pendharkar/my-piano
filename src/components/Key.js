1d5238f0b3f98a48b97457ad0e6ac6707498a4f2
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-09-29 09:54:00 -0700

    Create Key component.

---
+++ src/components/Key.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Key extends React.Compoent {
+    render(){
+        return(
+            <div>
+                <h1>Key</h1>
+            </div>
+        )
+    }
+}
+
+export default Key;
