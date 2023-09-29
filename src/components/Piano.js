966fafcf840f9d9f7741d9a3c32b939c3a9c7832
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-09-29 10:13:43 -0700

    Map const NOTES in Piano.js.

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import Key from './Key';
 import '../styles/Piano.css';
+import NOTES from '../global/constants';
 
 class Piano extends React.Component{
     render(){
+        const keys = NOTES.map(note => {
+            return <Key note={note} key={note} />
+        })
         return(
             <div className="piano">
-                <Key note="c"/>
-                <Key note="df"/>
-                <Key note="d"/>
-                <Key note="ef"/>
-                <Key note="e"/>
-                <Key note="f"/>
-                <Key note="gf"/>
-                <Key note="g"/>
-                <Key note="af"/>
-                <Key note="a"/>
-                <Key note="bf"/>
-                <Key note="b"/>
-                <Key note="c2"/>
+                {keys}
             </div>
         )
     }
