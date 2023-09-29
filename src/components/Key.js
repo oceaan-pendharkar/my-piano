03ec806dc410c400148152177773e88a4d1042cc
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-09-29 11:42:35 -0700

    Add buttons for click events.

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,9 +1,14 @@
 import '../styles/Key.css';
 
 export default function Key(props) {
+    function handleClick() {
+        alert("You clicked a key!");
+    }
     return (
         <div className={props.className}>
-            <div className="note"></div>
+            <button className="note" onClick={handleClick}>
+                <p className="note-name">{props.note}</p>
+            </button>
         </div>
     )
 }
