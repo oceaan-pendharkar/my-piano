a6247c114f8f644ce555d5e10f4ba3a50ebf06e7
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-09-29 09:49:38 -0700

    Create piano component.

---
+++ src/components/Piano.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Piano extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Piano</h1>
+            </div>
+        )
+    }
+}
+
+export default Piano;
