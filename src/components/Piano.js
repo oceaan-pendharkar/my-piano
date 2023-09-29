845eb4d7198e66ed21294a6aa6326ce7a9b5c6eb
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-09-29 10:08:45 -0700

    Add key names to keys in Piano.js.

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -6,9 +6,19 @@ class Piano extends React.Component{
     render(){
         return(
             <div className="piano">
-                <Key />
-                <Key />
-                <Key />
+                <Key note="c"/>
+                <Key note="df"/>
+                <Key note="d"/>
+                <Key note="ef"/>
+                <Key note="e"/>
+                <Key note="f"/>
+                <Key note="gf"/>
+                <Key note="g"/>
+                <Key note="af"/>
+                <Key note="a"/>
+                <Key note="bf"/>
+                <Key note="b"/>
+                <Key note="c2"/>
             </div>
         )
     }
