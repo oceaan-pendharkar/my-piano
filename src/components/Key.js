5a78caabd8397f3701822b8ecbe055b2878ded32
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-09-29 09:58:48 -0700

    Edit Key class.

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import './Key.css';
 
 class Key extends React.Component {
     render(){
         return(
-            <div>
-                <h1>Key</h1>
-            </div>
+            <div className="key"/>
         )
     }
 }
