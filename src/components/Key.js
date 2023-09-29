c692be53130f12df4ff331ee4e55e9839b5b0f53
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-09-29 10:07:28 -0700

    Include Key name in Key component.

--- src/components/Key.js
+++ src/components/Key.js
@@ -4,7 +4,11 @@ import '../styles/Key.css';
 class Key extends React.Component {
     render(){
         return(
-            <div className="key"/>
+            <div className="key">
+                <div className="key-text">
+                    {this.props.note.toUpperCase()}
+                </div>
+            </div>
         )
     }
 }
